60eaa3b0e31e8a0c33ab153bf680cd4c941b0b44
SungWoo824 <scody1219@gmail.com>
2021-05-21 20:41:05 +0900

    add : servicePractice

---
+++ servicepractice/src/main/java/com/sw/servicepractice/ServicepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sw.servicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicepracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicepracticeApplication.class, args);
+    }
+
+}
---
+++ servicepractice/src/test/java/com/sw/servicepractice/ServicepracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.servicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicepracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
